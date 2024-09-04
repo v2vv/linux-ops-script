a73f2e494d0157559887dde1f72692af21d08952
v2vv <v2vvcn@gmail.com>
2024-09-04 20:18:28 +0800

    增加 remote.sh 软件检测与安装

--- remote.sh
+++ remote.sh
@@ -11,6 +11,7 @@ OS_TYPE=$(uname)
 hosttag=""
 option="uptime-kuma"
 script=""
+system_env=""
 
 while getopts ":n:h" opt; do
   case ${opt} in
@@ -124,6 +125,25 @@ echo "主机: $hosttag, 操作: $action",  软件: $option
 # 在这里执行你的操作
 
 
+check_soft_env(){
+    # 检查是否存在 jq 命令
+    echo "--- 检查 jq 是否已安装 ---"
+    if command -v jq >/dev/null 2>&1; then
+        echo "jq 工具已安装"
+    else
+        echo "未检测到 jq 存在"
+        if [[ "$OS_TYPE" == "Linux" ]]; then
+            echo "当前系统类型为 $OS_TYPE 正在通过 apt 指令安装 jq"
+            apt install jq -y
+        elif [[ "$OS_TYPE" == "MINGW"* || "$OS_TYPE" == "CYGWIN"* ]]; then
+            echo "当前系统类型为 $OS_TYPE 正在通过 winget 安装 jq"
+            winget install jqlang.jq          
+        fi
+    fi
+}
+
+
+
 # $1 password
 sshcommand(){
     local host=$1
@@ -163,6 +183,9 @@ scpcommand(){
         exit 1
     fi
 }
+
+# 检查 jq 软件是否安装
+check_soft_env
 
 hostname=$(jq -r ".ungrouped.hosts.$hosttag.ansible_host" host.json)
 hostuser=$(jq -r ".ungrouped.hosts.$hosttag.ansible_user" host.json)
