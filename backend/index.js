4c8afd9551a8aace1de1edf5958196d7b014aea2
v2vvcn <v2vvcn@gmail.com>
2025-03-28 13:37:30 +0000

    解决 CORS

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { exec } = require('child_process');
+const cors = require('cors');  // Import cors
 const app = express();
 const port = 3000;
 
 // 使用 express 内置的 JSON 解析中间件
 app.use(express.json());
+
+// Enable CORS for all origins or you can specify the origin
+app.use(cors());  // This will allow all domains, or you can customize it
 
 // 定义 POST 接口
 app.post('/run-command', (req, res) => {
